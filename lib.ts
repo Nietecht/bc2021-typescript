c875590c16ac68b57760b457c77962080f6b4ca0
Ruben Goris <rubengoris@gmail.com>
2021-10-19 19:54:38 +0200

    Namespace materialization sample

--- lib.ts
+++ lib.ts
@@ -1,3 +1,11 @@
 export function capitalize(str: string) {
   return str.substr(0, 1).toLocaleUpperCase() + str.substr(1);
 }
+
+export namespace Foo {
+  export type CarName = string
+  
+  export enum FooEnum {
+    FooValue
+  }
+}
